d19fbdbc9b1ea1303484bf78304e0dc5e190b04b
unknown <studentmehedi214@gmail.com>
2023-08-18 17:47:43 +0600

    this is a main  section and area calculate

--- js/area-calcutal.js
+++ js/area-calcutal.js
@@ -8,10 +8,54 @@ function calculateTraingleArea(){
     const heightField = document.getElementById('triangle-height');
     const heightValueText = heightField.value;
     const heightValue = parseFloat(heightValueText);
+   // CalculateBuy
+
     const area = 0.5 * baseValue * heightValue;
-    console.log(area)
+ 
 //      show triangle area 
   const areaSpan = document.getElementById('translate-area');
   areaSpan.innerText = area;
 
+}
+
+function calculateRectangleArea(){
+ // get rectangle width value
+   const  widthField = document.getElementById('rectangle-width');
+   const widthValueText = widthField.value ;
+   const width = parseFloat(widthValueText);
+
+
+   // get rectangle length value
+   const lengthField = document.getElementById('rectangle-length');
+   const lengthValueText = lengthField.value ;
+   const length = parseFloat(lengthValueText);
+
+   //CalculateBuy
+   const area2 = width * length;
+
+//      show triangle area 
+     const areaSpan2 = document.getElementById('rectangle-area');
+     areaSpan2.innerText = area2
+
+}
+
+
+function calculateParallelogramArea(){
+   const parallelogramBase = getInputField('parallelogram-base');
+   const parallelogramHeight = getInputField('parallelogram-height');
+   const area = parallelogramBase * parallelogramHeight;
+     setInputElement('parallelogram-area', area);
+  
+}
+
+function getInputField(InputField){
+    const InputFieldText = document.getElementById(InputField);
+  const getInputValueText= InputFieldText .value;
+  const value = parseFloat(getInputValueText);
+  return value;
+}
+
+function setInputElement(Element, newInput){
+   const allElementText = document.getElementById(Element);
+   allElementText.innerText = newInput;
 }
